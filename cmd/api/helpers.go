dabaae554d60a03641eef039493171ed27d5a1c2
ASsssker <ask.mol2@mail.ru>
2024-06-29 22:38:49 +0300

    change healtchecker handler response to JSON format

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -13,8 +14,26 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 
 	id, err := strconv.ParseInt(param, 10, 64)
 	if err != nil || id < 1 {
-		return 0, errors.New("Invalid id parameter")
+		return 0, errors.New("invalid id parameter")
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return nil
 }
