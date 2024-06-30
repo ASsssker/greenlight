47119dadf614d44140388849c9688b2de703fdc9
ASsssker <ask.mol2@mail.ru>
2024-06-30 21:06:58 +0300

    added validation helper funcs

---
+++ internal/validator/validator.go
@@ -0,0 +1,57 @@
+package validator
+
+import (
+	"regexp"
+)
+
+var (
+	EmailRX = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+)
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func In[T comparable](value T, list ...T) bool {
+	for i:= range list {
+		if value == list[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Matches(values string, rx *regexp.Regexp) bool {
+	return rx.MatchString(values)
+}
+
+func Unique(values []string) bool {
+	uniqueValues := make(map[string]bool)
+
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(values) == len(uniqueValues)
+}
